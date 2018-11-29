3265f0a25d967cc3b45f420b901d856225c3b82f
Andrew Chan Mak <andrew.cm.sp@gmail.com>
2018-11-29 20:16:58 -0200

    ajsute
    ajuste

--- vue-app/src/Firebase.js
+++ vue-app/src/Firebase.js
@@ -3,12 +3,12 @@ import firestore from 'firebase/firestore'
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-  apiKey: "AIzaSyA0GKZ8fmq91G6JTYGU8AEeglsm3WFAcko",
-  authDomain: "vue-auth-firebase-466b2.firebaseapp.com",
-  databaseURL: "https://vue-auth-firebase-466b2.firebaseio.com",
-  projectId: "vue-auth-firebase-466b2",
-  storageBucket: "vue-auth-firebase-466b2.appspot.com",
-  messagingSenderId: "654139907214"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 
 firebase.initializeApp(config);
